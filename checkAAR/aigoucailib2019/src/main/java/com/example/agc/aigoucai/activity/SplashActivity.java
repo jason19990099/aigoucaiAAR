464dbbc03d2d37e15978e6cb5408aee61a4d9e32
DESKTOP-VFHMO2M\o <jason19990099@gmail.com>
2019-01-07 20:14:10 +0800

    123

--- checkAAR/aigoucailib2019/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
+++ checkAAR/aigoucailib2019/src/main/java/com/example/agc/aigoucai/activity/SplashActivity.java
@@ -4,11 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 
 import com.example.agc.aigoucai.R;
 import com.example.agc.aigoucai.util.LogUtil;
-import com.example.agc.aigoucai.util.SharePreferencesUtil;
 import com.example.agc.aigoucai.util.SocketUtil;
 import com.example.agc.aigoucai.util.SystemUtil;
 
@@ -22,49 +20,12 @@ import java.util.List;
  * 启动页
  */
 public class SplashActivity extends AppCompatActivity {
-    ImageView ivWelcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_splash);
 
         SystemUtil.setfullScreen(SplashActivity.this);
-//        ivWelcome=findViewById(R.id.iv_welcome2);
-
-//        String getintent = SharePreferencesUtil.getString(SplashActivity.this, "getIntent", "");
-//        if(getintent.contains("com.500CPActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.backgroud_500cp));
-//        }
-//        if(getintent.contains("com.AigoucaiActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_aigoucai));
-//        }
-//        if(getintent.contains("com.k7Activity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_k7));
-//        }
-//        if(getintent.contains("com.ttActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_tt));
-//        }
-//        if(getintent.contains("com.678yuleActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_678));
-//        }
-//        if(getintent.contains("com.xpjActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_xpj));
-//        }
-//        if(getintent.contains("com.zzcActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_zzc));
-//        }
-//        if(getintent.contains("com.egoActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_egouwangtou));
-//        }
-//        if(getintent.contains("com.zxcActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_axc));
-//        }
-//        if(getintent.contains("com.8HaoActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_8hao));
-//        }
-//        if(getintent.contains("com.pandaActivity")){
-//            ivWelcome.setImageDrawable(getResources().getDrawable(R.mipmap.background_panda));
-//        }
 
         List<String> ip_array = (List<String>) getIntent().getSerializableExtra("ip_array");
 
